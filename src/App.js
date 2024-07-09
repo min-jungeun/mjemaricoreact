af545880658257812f22d9404f3bae7cd6bf7f3e
min-jungeun <mje971115@gmail.com>
2024-07-09 12:41:01 +0900

    마리코 국내여행추천지 연결

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='font'>폰트를 넣었다이</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Mje from './component/Mje';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './scss/fonts.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <Mje></Mje>
+ 
+);
+
+
